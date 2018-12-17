3ccfc55f563f41c8d8ebd6c7bac212b532eb820a
Artiom <arti2895@gmail.com>
2018-12-17 23:47:28 +0100

    Add JS file, Modify #collapsing time

---
+++ mainScript.js
@@ -0,0 +1,7 @@
+$('#collapseOne').on('show.bs.collapse', function () {
+    $('#seeMoreText').text('...Pokaż Mniej');
+  })
+
+$('#collapseOne').on('hide.bs.collapse', function () {
+    $('#seeMoreText').text('Pokaż Więcej...');
+  })
